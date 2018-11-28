a0916828863d39e6f938895c757e1f948572e837
sss-0916 <sss-0916@qq.com>
2018-11-28 08:07:10 +0800

    2018_11_28

---
+++ 换行和回车的区别.c
@@ -0,0 +1,16 @@
+/*
+**	换行'\n'和回车'\r'的区别
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+	int 数组[1] = { 0 };
+	printf("%d\n", 数组[0]);
+	system("pause");
+	return 0;
+}
