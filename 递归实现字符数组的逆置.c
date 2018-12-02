c90476b6e6ded9358f67c76f19d18c7a071f428f
sss-0916 <sss-0916@qq.com>
2018-12-02 08:10:44 +0800

    2018_12_02

---
+++ 递归实现字符数组的逆置.c
@@ -0,0 +1,46 @@
+/*
+**	??????????????????
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "stdio.h"
+#include "stdlib.h"
+
+/*---?????????????---*/
+int stringLength(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		++count;
+		++str;
+	}
+	return count;
+}
+
+/*---????????????---*/
+void reverse_string(char* str)
+{
+	char temp = 0;
+	if (*str != '\0')
+	{
+		char* start = str;
+		char* end = str + stringLength(str) - 1;
+		temp = *start;
+		*start = *end;
+		*end = '\0';
+		reverse_string(str + 1);
+		*end = temp;
+	}
+}
+
+int main()
+{
+	char str[] = "dlroWolleH";
+	printf("??????????????: %s\n", str);
+	reverse_string(str);
+	printf("???¨²?????????: %s\n", str);
+	system("pause");
+	return 0;
+}
