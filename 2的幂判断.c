abb012711e2c9227ec22da9f29afdbff3445fa5b
sss-0916 <sss-0916@qq.com>
2018-12-26 07:59:45 +0800

    2018_12_26

---
+++ 2的幂判断.c
@@ -0,0 +1,41 @@
+/*
+**	是否为2的幂
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "stdio.h"
+#include "stdlib.h"
+
+//2的幂判断
+int isPowOfTwo(int n)
+{
+	if (n <= 0)
+	{
+		return 0;
+	}
+	if ((n & (n - 1)) == 0)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+int main()
+{
+	int num = 0;
+	int flag = 0;
+	printf("请输入一个数: \n");
+	scanf("%d", &num);
+	flag = isPowOfTwo(num);
+	if (flag == 1)
+	{
+		printf("是2的幂!\n");
+	}
+	else
+	{
+		printf("不是2的幂!\n");
+	}
+	system("pause");
+	return 0;
+}
