9b28b39e41d2a5dcc7b695a1ee447f335f274234
sss-0916 <sss-0916@qq.com>
2018-12-29 07:52:33 +0800

    2018_12_29

---
+++ 是否是完全平方数.c
@@ -0,0 +1,54 @@
+/*
+**	完全平方数的判断
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "stdio.h"
+#include "stdlib.h"
+
+//完全平方数的判断
+int isPerfectSquare(int n)
+{
+	if (n < 0)
+	{
+		return 0;
+	}
+	if (n == 0 || n == 1)
+	{
+		return 1;
+	}
+	int i = 1;
+	int j = n / 2;
+	while (i <= j)
+	{
+		if (i * i == n)
+		{
+			return 1;
+		}
+		else
+		{
+			++i;
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	int num = 0;
+	int flag = 0;
+	printf("请输入一个数: \n");
+	scanf("%d", &num);
+	flag = isPerfectSquare(num);
+	if (flag == 1)
+	{
+		printf("是完全平方数!\n");
+	}
+	else
+	{
+		printf("不是完全平方数!\n");
+	}
+	system("pause");
+	return 0;
+}
