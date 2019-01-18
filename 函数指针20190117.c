87a13849cda33d2b38392c62bd5614e5dfc79602
sss-0916 <sss-0916@qq.com>
2019-01-18 08:18:17 +0800

    2019_01_18

---
+++ 函数指针20190117.c
@@ -0,0 +1,25 @@
+/*
+**	º¯ÊýÖ¸Õë
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "stdio.h"
+#include "stdlib.h"
+
+void test()
+{
+	printf("hehe\n");
+}
+
+typedef void (*TestPointer)();
+
+int main()
+{
+	printf("%p\n", test);
+	printf("%p\n", &test);
+	TestPointer t1 = test;
+	t1();
+	system("pause");
+	return 0;
+}
