3875a1323ae8f6859a445fe11c3d8e06d89e0812
sss-0916 <sss-0916@qq.com>
2019-02-23 08:53:11 +0800

    2019_02_23

---
+++ 模拟实现strchr20190221.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define N 1024
+
+const char* myStrchr(const char* str, char c)
+{
+	assert(str != NULL);
+	while (*str != '\0')
+	{
+		if (*str == c)
+		{
+			return str;
+		}
+		++str;
+	}
+	return NULL;
+}
+
+int main()
+{
+	char str[N] = "hello, world!";
+	printf("%s\n", myStrchr(str, 'w'));
+	system("pause");
+	return 0;
+}
