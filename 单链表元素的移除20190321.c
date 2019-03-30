9ecc4f291f9917cd3fe217c29bb44bd1efbf3c48
sss-0916 <sss-0916@qq.com>
2019-03-30 13:43:10 +0800

    2019_03_30

---
+++ 单链表元素的移除20190321.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef int ElementType;
+
+/*---½áµã---*/
+typedef struct Node 
+{
+	ElementType value;
+	struct Node* next;
+} Node;
+
+Node* sLLRemove(Node** first, ElementType v)
+{
+	assert(first != NULL);
+	if (*first == NULL)
+	{
+		return *first;
+	}
+	Node* cur = *first;
+	while (cur->next != NULL)
+	{
+		if (cur->next->value == v)
+		{
+			Node* temp = cur->next->next;
+			free(cur->next);
+			cur->next = temp;
+		}
+		else
+		{
+			cur = cur->next;
+		}
+	}
+	if ((*first)->value == v)
+	{
+		Node* temp = (*first)->next;
+		free(*first);
+		*first = temp;
+	}
+	return *first;
+}
