81d2957b09bef95cf98f5d86df35886892eee9e7
sss-0916 <sss-0916@qq.com>
2019-04-11 07:42:30 +0800

    2019_04_11

---
+++ 合并两个单链表20190321.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef int ElementType;
+
+/*---½áµã---*/
+typedef struct Node 
+{
+	ElementType value;
+	struct Node* next;
+} Node;
+
+Node* mergeTwoList(Node** head1, Node** head2)
+{
+	assert(head1 != NULL);
+	assert(head2 != NULL);
+	if (*head1 == NULL)
+	{
+		return *head2;
+	}
+	if (*head2 == NULL)
+	{
+		return *head1;
+	}
+	Node* first = *head1;
+	Node* second = *head2;
+	Node* result = NULL;
+	Node* tail = NULL;
+	while (first != NULL && second != NULL)
+	{
+		if (first->value <= second->value)
+		{
+			if (tail != NULL)
+			{
+				tail->next = first;
+				tail = first;
+			}
+			else
+			{
+				result = tail = first;
+			}
+		}
+		else
+		{
+			if (tail != NULL)
+			{
+				tail->next = second;
+				tail = second;
+			}
+			else
+			{
+				result = tail = second;
+			}
+		}
+	}
+	if (first == NULL)
+	{
+		tail->next = second;
+	}
+	if (second == NULL)
+	{
+		tail->next = first;
+	}
+	return result;
+}
